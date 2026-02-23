7aa4d7d04346abdfbd69a48197bf2723a0369cc3
Dariusz Sikora <dev@isangeles.pl>
2026-02-23 23:12:15 +0100

    example for checkslot

---
+++ example/checkslot/main.go
@@ -0,0 +1,77 @@
+/*
+ * main.go
+ *
+ * Copyright 2026 Dariusz Sikora <ds@isangeles.dev>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ *
+ *
+ */
+
+// Example for MTK check slot.
+package main
+
+import (
+	"fmt"
+
+	"golang.org/x/image/colornames"
+
+	"github.com/gopxl/pixel"
+	"github.com/gopxl/pixel/pixelgl"
+
+	"github.com/isangeles/mtk"
+)
+
+// Main function.
+func main() {
+	// Run Pixel graphic.
+	pixelgl.Run(run)
+}
+
+// All window code fired from there.
+func run() {
+	// Create Pixel window configuration.
+	cfg := pixelgl.WindowConfig{
+		Title:  "MTK Check Slot example",
+		Bounds: pixel.R(0, 0, 1600, 900),
+	}
+	// Create MTK warpper for Pixel window.
+	win, err := mtk.NewWindow(cfg)
+	if err != nil {
+		panic(fmt.Errorf("Unable to create MTK window: %v", err))
+	}
+	// Create the check slot.
+	slot := mtk.NewCheckSlot("Check/Uncheck", false, mtk.SizeBig.ButtonSize(mtk.ShapeRectangle), colornames.Red, colornames.Blue)
+	// Set function for slot check event.
+	slot.SetOnCheckFunc(onSlotChecked)
+	// Main loop.
+	for !win.Closed() {
+		// Clear window.
+		win.Clear(colornames.Black)
+		// Draw button.
+		slotPos := win.Bounds().Center()
+		slot.Draw(win, mtk.Matrix().Moved(slotPos))
+		// Update.
+		win.Update()
+		slot.Update(win)
+	}
+}
+
+// onSlotChecked handles slot check
+// event.
+func onSlotChecked(cs *mtk.CheckSlot) {
+	fmt.Println("Check!")
+}
